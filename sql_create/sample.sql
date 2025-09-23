14c2e703a44479fcf248ed2ef56ba09b0fe88e70
Hailey Hanson <haileylhanson@gmail.com>
2025-09-23 10:25:09 -0700

    created data and sql_create folders and put sample sql & db files into sql_create which create a sqlite database of authors

---
+++ sql_create/sample.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE authors (
+--     author_id TEXT PRIMARY KEY,
+--     first TEXT NOT NULL,
+--     last TEXT NOT NULL
+-- );
+
+-- INSERT INTO authors(author_id, first, last)
+-- VALUES ("10f88232-1ae7-4d88-a6a2-dfcebb22a596", "Harper" , "Lee");
+
+-- INSERT INTO authors(author_id, first, last)
+-- VALUES ("8d8107b6-1f24-481c-8a21-7d72b13b59b5", "J.D." , "Salinger");
+
+SELECT * FROM authors;
